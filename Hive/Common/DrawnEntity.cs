933b7dbd113b06cae24d1d76cd9e7468cb78a183
Lonely-Taco <47434636+Lonely-Taco@users.noreply.github.com>
2023-02-28 16:12:56 +0100

    add entities

---
+++ Hive/Common/DrawnEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Hive.Common
+{
+    public abstract class DrawnEntity : IEntity
+    {
+        protected Texture2D texture;
+        protected Vector2 position;
+
+        protected DrawnEntity(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+
+        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+
+        public abstract void Update(GameTime gameTime);
+
+    }
+}
