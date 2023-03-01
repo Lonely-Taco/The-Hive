ee07dbb2a8dbd7c8d9ce4d16287837425bb5405b
Thomas Koops <33625069+ThomasK24a@users.noreply.github.com>
2023-03-01 13:04:39 +0100

    Add shop

---
+++ Hive/Shop/ExpansionShop.cs
@@ -0,0 +1,30 @@
+﻿using Hive.Utility;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.Shop
+{
+    internal class ExpansionShop : Shop
+    {
+        private Counter expansionCounter;
+        public ExpansionShop(Texture2D icon, Counter expansionCounter, Counter nectarCounter) : base(icon, nectarCounter)
+        {
+            this.expansionCounter = expansionCounter;
+        }
+
+        public async override void Buy()
+        {
+            int expansionLevel = await expansionCounter.AddCount(1);
+            await nectarCounter.AddCount(-CurrentCost(expansionLevel));
+        }
+
+        public override int CurrentCost(int currentAmount)
+        {
+            return currentAmount * 5; //TODO: change later
+        }
+    }
+}
---
+++ Hive/Shop/Shop.cs
@@ -0,0 +1,38 @@
+﻿using Hive.Common;
+using Hive.Utility;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.Shop
+{
+    public abstract class Shop : IEntity
+    {
+        protected Texture2D icon;
+        protected Counter nectarCounter;
+
+        protected Shop(Texture2D icon, Counter nectarCounter)
+        {
+            this.icon = icon;
+            this.nectarCounter = nectarCounter;
+        }
+
+        public abstract int CurrentCost(int currentAmount);
+
+        public abstract void Buy();
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
