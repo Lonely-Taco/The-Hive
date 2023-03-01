21ade26864363e4561dcbd74f430b178f2420f1e
Lonely-Taco <47434636+Lonely-Taco@users.noreply.github.com>
2023-03-01 16:40:01 +0100

    Add hive classes

---
+++ Hive/Map/AntObject.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace Hive.Map
+{
+    internal class AntObject : HiveMapObject
+    {
+        private Vector2 currentDestination;
+        private int speed;
+
+        public AntObject(int speed, Vector2 mapCoordinates, Texture2D texture, Vector2 position) : base(mapCoordinates, texture, position)
+        {
+            this.speed = speed;
+        }
+
+        public NectarObject GetNearestNectar(List<NectarObject> nectarObjects)
+        {
+            float shortestDistance = float.MaxValue;
+            NectarObject nearestNectar = null;
+            foreach(NectarObject nectarObject in nectarObjects) 
+            {
+                float distance = Vector2.Distance(nectarObject.GetMapCoordinates(), this.GetMapCoordinates());
+                if (distance < shortestDistance)
+                {
+                    shortestDistance = distance;
+                    nearestNectar = nectarObject;
+                }
+            }
+            return nearestNectar;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        internal void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Hive/Map/HiveMapObject.cs
@@ -0,0 +1,26 @@
+﻿using Hive.Common;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.Map
+{
+    public abstract class HiveMapObject : DrawnEntity
+    {
+        protected Vector2 mapCoordinates;
+
+        protected HiveMapObject(Vector2 mapCoordinates, Texture2D texture, Vector2 position) : base(texture, position)
+        {
+            this.mapCoordinates = mapCoordinates;
+        }
+
+        public Vector2 GetMapCoordinates()
+        {
+            return this.mapCoordinates;   
+        }
+    }
+}
